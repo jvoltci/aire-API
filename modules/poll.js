b88ef46bcce99ae655e15cf329bfae8aa1e00454
jvoltci <jvoltci@gmail.com>
2019-08-25 21:33:28 +0530

    Testing...

--- modules/poll.js
+++ modules/poll.js
@@ -64,31 +64,18 @@ class Poll {
 	}
 	handlePseudonym(req, res) {
 		const { pseudonym } = req.body;
-		polls.forEach(data => {
+		Object.keys(pollsData).forEach(data => {
 			if(unit === pseudonym)
 				res.json({isAvailable: false});
 		})
 
-		polls.push(pseudonym);
+		pollsData[pseudonym] = {};
 		res.json({isAvailable: true})
 
 	}
 	removePoll(pseudonym) {
-		delete pollsData.pseudonym;
-		for(let i = 0; i < polls.length; ++i)
-			if(polls[i] === pseudonym) {
-				polls.splice(i, 1);
-
-				const tempPolls = polls.map((unit) => {
-					return {
-						isSecure: pollsData.unit.isSecure,
-						pseudonym: unit,
-					}
-				});
-				delete pollsData.pseudonym;
-				this.io.sockets.emit('live polls', tempPolls);
-				break;
-			}
+		delete pollsData[pseudonym];
+		this.io.sockets.emit('live polls', this.curatePolllList());
 	}
 	updatelist(data) {
 		pollsData[data.pseudonym].listParticipants[data.index] = data.name;
