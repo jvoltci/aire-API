67abf14b1dd011e3f17468ff3ba708d1beaec023
jvoltci <jvoltci@gmail.com>
2019-08-28 00:52:35 +0530

    Testing...

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use('/node_modules',  express.static( path.join(__dirname, '/node_modules')));
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://alre.ml");
+  res.header("Access-Control-Allow-Origin", "https://alree.ml");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
