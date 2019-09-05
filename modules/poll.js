b3bdeed51747d3f1632fdd9402dda58eae556067
jvoltci <jvoltci@gmail.com>
2019-09-05 07:20:51 +0530

    Testing...

--- modules/poll.js
+++ modules/poll.js
@@ -123,6 +123,7 @@ class Poll {
 	listParticipants(req, res) {
 		const { pseudonym } = req.body;
 		let list = {};
+		console.log(this.nodes.list)
 		this.nodes.list.forEach(pUser => {
 			if(pUser.pseudonym === pseudonym && pUser.isPolling)
 				list = pUser.listParticipants;
