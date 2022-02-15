15cdedc14efaee00e321e9cc0d282a56c85beb2b
J.AI <jvoltci@gmail.com>
2022-02-16 00:32:03 +0530

    Updated CORS

--- server.js
+++ server.js
@@ -28,7 +28,12 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use('/node_modules',  express.static( path.join(__dirname, '/node_modules')));
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://alre.ml");
+  const allowedOrigins = ['https://jvoltci.github.io/aire', 'https://aire.ivehement.com'];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+       res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.header("Access-Control-Allow-Origin", origin);
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
